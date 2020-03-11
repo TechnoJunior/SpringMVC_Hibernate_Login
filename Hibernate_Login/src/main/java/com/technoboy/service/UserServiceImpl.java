dfdaeb0a789aaf9aee422b1e751ea77af2573198
Techn <Techn@DESKTOP-59OHU1T>
2020-03-11 21:43:27 +0530

    configured userserviceimpl

--- Hibernate_Login/src/main/java/com/technoboy/service/UserServiceImpl.java
+++ Hibernate_Login/src/main/java/com/technoboy/service/UserServiceImpl.java
@@ -3,16 +3,20 @@ package com.technoboy.service;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.technoboy.entity.User;
 import com.technoboy.model.UserDAO;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDAO userDao;
 	
 	@Override
+	@Transactional
 	public boolean login(User user) throws SQLException {
 		return userDao.Login(user);
 	}
